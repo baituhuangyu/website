b3daf1bb66ebaa66cd3a3ed6774245e7d3216ae3
baituhuangyu <baituhuangyu@qq.com>
2015-05-21 21:12:44 +0800

    python+jango+sqllite+databases+blog
    这是一个用python语言写的一个基于django框架的blog项目，其数据库是sqllite，并带有admin管理

---
+++ website/blog/views.py
@@ -0,0 +1,10 @@
+#from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.template import loader,Context
+
+def index(request):
+	t = loader.get_template("index.html")
+	c = Context({})
+	return HttpResponse(t.render(c))
